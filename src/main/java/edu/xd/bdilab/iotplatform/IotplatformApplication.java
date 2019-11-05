a5d9069d43cea9fcd4afd5f4fb349663fdac01dd
shenlanjizou <835060761@qq.com>
2019-11-05 21:37:15 +0800

    11/5 21:37

--- src/main/java/edu/xd/bdilab/iotplatform/IotplatformApplication.java
+++ src/main/java/edu/xd/bdilab/iotplatform/IotplatformApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class IotplatformApplication {
 
     public static void main(String[] args) {
--- src/main/java/edu/xd/bdilab/iotplatform/service/security/UserDetailsServiceImpl.java
+++ src/main/java/edu/xd/bdilab/iotplatform/service/security/UserDetailsServiceImpl.java
@@ -17,6 +17,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
